f59fac5986c57b6911aaecae938521c4b59f3a79
JQZ1111 <jiaqi.zhao@polymtl.ca>
2023-09-07 21:18:02 -0400

    Added test to check for group velocity matching

---
+++ inv_design/tests/test_initialization.py
@@ -0,0 +1,18 @@
+from inv_design import get_initialization_cond as init
+import numpy as np
+
+np.random.seed(0)
+
+def test_GVD():
+    """
+    Test for the function get_constants from get_initialization for inverse design.
+    Fails if the group velocity matching of signal and idler is not respected
+    """
+    vp = 5*np.random.rand(1)
+    l = np.random.rand(1)
+    wi = -3
+    wf = 3
+    Np = np.random.rand(1)
+    alpha_phase = 2*np.pi*np.random.rand(1)
+    alpha, G, H = init.get_constants(vp, l, wi, wf, Np, alpha_phase)
+    assert jnp.allclose(G + H, np.diag((-2/vp)*jnp.linspace(wi, wf, len(G))))
