c0ae2c29447612a34d31ed5a34acbc656437ecb0
JQZ1111 <jiaqi.zhao@polymtl.ca>
2024-08-27 11:29:35 -0400

    Added JIT compilation

--- inv_design/optimization_quadratures.py
+++ inv_design/optimization_quadratures.py
@@ -12,7 +12,7 @@ def get_hankel_matrix(pump, N_omega):
     Returns the real valued matrix representing the pump.
     
     Args: 
-        pump[float]: problem parameters. Length is 2*N_omega - 1
+        pump[float]: problem parameters, length is 2*N_omega - 1
         N_omega(int): dimension of the propagators
     returns:
         array[float]: Hankel matrix representing the pump
@@ -25,7 +25,7 @@ def get_propagators(pump, N_omega, delta_k, l):
     Returns the propagators for the equations of dynamics with pseudo-quadratures
     
     Args:
-        pump[float]: problem parameters. Length is 2*N_omega - 1
+        pump[float]: problem parameters, length is 2*N_omega - 1
         N_omega(int): dimension of the propagators
         delta_k [[complex]]: matrix containing the elements about the symmetric-group-velocity matching
         l (float): length of the waveguide
@@ -39,12 +39,12 @@ def get_observables(pump, N_omega, delta_k, l):
     Returns the mean number of photon pairs per pulse and the Schmidt number
     
     Args:
-        pump[float]: problem parameters. Length is 2*N_omega - 1
+        pump[float]: problem parameters, length is 2*N_omega - 1
         N_omega(int): dimension of the propagators
         delta_k [[complex]]: matrix containing the elements about the symmetric-group-velocity matching
         l(float): length of the waveguide
     """
-    W_plus, W_minus = get_propagators(pump, N_omega, delta_k, l)
+    W_plus, W_minus = jax.jit(get_propagators, static_argnums=(1,))(pump, N_omega, delta_k, l)
     J = 0.25*(W_plus.conj().T@W_plus + W_minus.conj().T@W_minus - 2*jnp.eye(N_omega))
     N = jnp.real(jnp.trace(J))
     K = jnp.real((N**2)/jnp.trace(J.conj().T@J))
@@ -55,7 +55,7 @@ def problem(pump, omega, delta_k, l, y_N, k):
     Gives the penalized objective function
 
     Args:
-        pump[float]: problem parameters. Length is 2*N_omega - 1
+        pump[float]: problem parameters, length is 2*N_omega - 1
         omega[float]: discretized frequency domain
         alpha(float): constant including power of pump, group velocity of all modes, etc.
         H[[float]]: matrix containing the elements that are not in the integral in the dynamics equations
@@ -68,5 +68,3 @@ def problem(pump, omega, delta_k, l, y_N, k):
     var_loss = ((jnp.linspace(omega[0], omega[-1], 2*N_omega - 1))**2)@jnp.abs(pump)/jnp.sum(jnp.abs(pump))
     loss = jnp.real(schmidt_number) - 1 + .01*var_loss
     return loss + k*(loss_n + mean_loss**2)
-
-
